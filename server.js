ee6430c873a18b2810e355b703fc49e75188d5bc
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2026-03-07 20:29:49 -0300

    improvement

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes/'));
-
-
+app.use('/', require('./routes/contacts'));
 
 mongodb.initDb((err) => {
     if (err) {
