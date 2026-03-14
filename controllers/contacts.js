704028604e132bf757ac370b39116ffa8d7d3cf3
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2026-03-14 17:13:24 -0300

    Week 2 - setp 1 (PUT, POST, DELETE)

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -14,7 +14,55 @@ const getContactById = async (req, res) => {
     res.status(200).json(result);
 };
 
+const createContact = async (req, res) => {
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongoDb.getDatabase().db().collection('contacts').insertOne(newContact);
+    if (response.acknowledged > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const updatedContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongoDb.getDatabase().db().collection('contacts').updateOne({_id: contactId}, {$set: updatedContact});
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const deleteContact = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongoDb.getDatabase().db().collection('contacts').deleteOne({_id: contactId}, true);
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+
 module.exports = {
     getAllContacts,
-    getContactById
+    getContactById,
+    createContact,
+    updateContact,
+    deleteContact
+
 };
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
-const mongodb = require('./data/database')
+const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes/contacts'));
+app.use(bodyParser.json());
+app.use('/', require('./routes/index'));
+
 
 mongodb.initDb((err) => {
     if (err) {
